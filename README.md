0c7645be048795387deb956be306bf5e11f8c966
bottomffeeder <karl.gersalia@gmail.com>
2026-06-29 07:13:59 +0000

    initial commit: and gate module and testbench

---
+++ README.md
@@ -0,0 +1,6 @@
+#VERILOG FILES
+
+Verilog modules and testbenches written and simulated using Icarus Verilog and GTKWave on Ubuntu Linux.
+##Modules
+-AND gate with testbench
+
---
+++ basics/and_gate_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+module and_gate_tb;
+
+reg a, b;
+wire y;
+
+and_gate uut (
+.a(a),
+.b(b),
+.y(y)
+);
+
+initial begin
+	$dumpfile("and_gate.vcd");
+	$dumpvars(0, and_gate_tb);
+end
+
+initial begin
+	a = 0; b = 0; #10;
+	a = 0; b = 1; #10;
+	a = 1; b = 0; #10;
+	a = 1; b = 1; #10;
+$finish;
+end
+
+initial begin
+$monitor ("time=%0t | a=%b b=%b | y=%b", $time, a, b, y);
+end
+
+endmodule
+
